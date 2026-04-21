8794fca14ce8cecf7376359ce36c1ba61f757eb8
John W. Carbone <jwc65535@gmail.com>
2026-04-21 16:35:24 -0400

    Update fixit script to automate sample package renaming
    Modifies the script to detect the "name" field in sample manifests and automatically update it with the correct `@framesquared/sample-` prefix based on the directory name.

--- samples/fixit.sh
+++ samples/fixit.sh
@@ -6,8 +6,12 @@ do
 	if [ -f $p ]
 	then
 		echo $p
-		grep name $p
+		if grep '"name"' $p
+		then
+		  echo OKAY
+		  sed -i 's/\("name": "@framesquared\/sample-\).*/\1'$f'",/' $p
+    fi
 	fi
 
 done
-		
+
